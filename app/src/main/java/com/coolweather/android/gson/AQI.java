68c79c3bf95cc4eb8b947c9a4d103e64a13f7dcd
Tony <304617547@qq.com>
2017-04-23 23:07:58 +0800

    加入显示天气信息。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/4/23.
+ */
+
+public class AQI {
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+    public AQICity city;
+}
